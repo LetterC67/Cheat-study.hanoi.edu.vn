3b0a28086fbdc8201b53aa2d82d33939c22d884f
TekNoCow <53361407+TekNoCow@users.noreply.github.com>
2020-03-07 13:00:49 +0700

    Code để tạo ra hàm cheat

---
+++ cheatCode.js
@@ -0,0 +1,5 @@
+var cheatCode = ""
+for(i=0;i<testResultLocalObject.length;i++)
+    cheatCode = cheatCode + "SaveUserAnswer('"+testResultLocalObject[i].QuestionId.toString()+"','"+testResultLocalObject[i].AnswerId.toString()+"');";
+cheatCode = cheatCode + "SubmitTestResultClick();"
+console.log(cheatCode)
