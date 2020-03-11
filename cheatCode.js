2f5069151499f4acfe635f94311cb0c8ed8a5629
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-03-11 22:39:13 +0700

    Update cheatCode.js

--- cheatCode.js
+++ cheatCode.js
@@ -2,4 +2,5 @@ var cheatCode = ""
 for(i=0;i<testResultLocalObject.length;i++)
     cheatCode = cheatCode + "SaveUserAnswer('"+testResultLocalObject[i].QuestionId.toString()+"','"+testResultLocalObject[i].AnswerId.toString()+"');";
 cheatCode = cheatCode + "SubmitTestResultClick();"
-console.log(cheatCode)
+console.log(cheatCode);
+console.log("Copy phần code trên! Paste vào console lần sau để có thời gian làm bài nhanh hơn!");
