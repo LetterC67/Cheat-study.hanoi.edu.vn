bb043ba4f1241aa346010a870118ac1a9a6cd0a0
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-03-27 23:05:46 +0700

    Update showAnswerKey.js
    Có thể Download đáp án về

--- showAnswerKey.js
+++ showAnswerKey.js
@@ -7,3 +7,35 @@ for(i=0;i<testResultLocalObject.length;i++){
         }
     }   
 }
+
+if (confirm('Bạn muốn tải file đáp án không?')) {
+    ques = document.getElementsByClassName("ImportWord-Normal");
+    _ques = [];
+
+    for(i=0;i<y.length;i++)
+        if(ques[i].innerText.replace(/\s/g, '').length)
+            _ques.push(y[i]);
+
+    data=""
+    for(i=0;i<_ques.length;i++)    
+        if(i%5==0)
+            data += (Math.ceil(i/5)+1).toString() + ". " + _ques[i].innerText+"\n";
+        else if(buttons[Math.floor(i/5)*4+i%5-1].checked)
+            data += "[x] " + _ques[i].innerText + "\n" ;
+        else data += "[ ] " + _ques[i].innerText + "\n";
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
+    element.setAttribute('download', "dap_an.txt");
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}else{
+    
+}'
+
+// * https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
