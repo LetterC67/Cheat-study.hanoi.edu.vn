4f25c2ee93e6c5eebae981475f7e4ff7c76fa919
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-04-08 17:38:55 +0700

    DEBUGGED!!! :D

--- getCorrectAnswers.js
+++ getCorrectAnswers.js
@@ -2,6 +2,13 @@ x = document.getElementsByClassName("question-box");
 ques = document.getElementsByClassName("col-11 question-box-title")
 ans = []
 for(i=0;i<x.length;i++){
+    if(x[i].getElementsByTagName("img").length == 4){
+        if(x[i].getElementsByClassName("text-primary")[0] != null)
+            ans.push([ques[i].innerText,x[i].getElementsByClassName("text-primary")[0].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt])
+        else
+            ans.push([ques[i].innerText,x[i].getElementsByClassName("text-success")[1].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt])
+        continue
+    }
     if(x[i].getElementsByClassName("text-primary")[0] != null)
         ans.push([ques[i].innerText,x[i].getElementsByClassName("text-primary")[0].parentNode.nextElementSibling.innerText])
     else
