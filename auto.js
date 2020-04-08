ffc37b0cdbe3bd4726ef67db10385a5736403329
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-04-08 11:33:55 +0700

    DEBUGGED!!! CODE MỚI - Tự động điền đáp án

--- auto.js
+++ auto.js
@@ -1,6 +1,15 @@
-var buttons = document.querySelectorAll('input[type="radio"]')
 ans = JSON.parse(localStorage.getItem("ans"))
-for(i=0;i<buttons.length;i++){
-    if(ans.includes(buttons[i].parentNode.nextElementSibling.innerText))
-        buttons[i].onclick();
+var buttons = document.querySelectorAll('input[type="radio"]')
+x = document.getElementsByClassName("question-box")
+for(i=0;i<x.length;i++){
+    ques = x[i].getElementsByClassName("col-11 question-box-title")[0].innerText;
+    var j=0;
+    for(j=0;j<ans.length;j++)
+        if(ques == ans[j][0])
+            break;
+    ansList = x[i].querySelectorAll('input[type="radio"]')
+	for(k=0;k<4;k++){
+        if(ans[j][1] == ansList[k].parentNode.nextElementSibling.innerText)
+            ansList[k].onclick()
+    }
 }
