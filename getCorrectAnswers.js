75cf954277d34d6427d1aa0063dfd31a1a883a32
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-04-08 11:34:41 +0700

    DEBUGGED!!! CODE MỚI - Lấy đáp án đúng

--- getCorrectAnswers.js
+++ getCorrectAnswers.js
@@ -1,10 +1,7 @@
-x = document.getElementsByClassName("text-primary")
+x = document.getElementsByClassName("question-box");
+ques = document.getElementsByClassName("col-11 question-box-title")
 ans = []
-for(i=1;i<x.length;i++){
-    ans.push(x[i].parentNode.nextElementSibling.innerText)
-}
-y = document.getElementsByClassName("text-success")
-for(i=2;i<y.length;i+=3){
-    ans.push(y[i].parentNode.nextElementSibling.innerText)
+for(i=0;i<x.length;i++){
+    ans.push([ques[i].innerText,x[i].getElementsByClassName("text-primary")[0].parentNode.nextElementSibling.innerText])
 }
 localStorage.setItem("ans",JSON.stringify(ans))
