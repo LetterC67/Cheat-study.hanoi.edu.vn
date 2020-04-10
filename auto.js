b4c9783d7b6e179c61ee737b2bfb1f474e139bb7
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-04-10 21:32:21 +0700

    DEBUGGED - Hỗ trợ đề toán

--- auto.js
+++ auto.js
@@ -3,11 +3,24 @@ var buttons = document.querySelectorAll('input[type="radio"]')
 x = document.getElementsByClassName("question-box")
 for(i=0;i<x.length;i++){
     ques = x[i].getElementsByClassName("col-11 question-box-title")[0].innerText.replace(/(\n| |\$)/gm, "");
-    var j=0;
-    for(j=0;j<ans.length;j++)
-        if(ques == ans[j][0])
-            break;
     ansList = x[i].querySelectorAll('input[type="radio"]')
+    ansList2 = []
+    for(l=0;l<4;l++){
+        if(ansList[l].parentNode.nextElementSibling.getElementsByTagName("img").length)
+            ansList2.push(ansList[l].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt)
+        else
+            ansList2.push(ansList[l].parentNode.nextElementSibling.innerText.replace(/(\n| )/gm, ""))
+    }
+    var j=0;
+    for(j=0;j<ans.length;j++){
+        if(ques == ans[j][0] && ansList2.includes(ans[j][1]))
+            break;
+    }
+    if(j==x.length){
+        console.log("Có vấn đề với câu " + (i+1).toString() + ". Bạn có thể tự làm lại câu đó nhé!")
+        continue
+    }
+   
     for(k=0;k<4;k++){
         if(ansList[k].parentNode.nextElementSibling.getElementsByTagName("img").length && ans[j][1] == ansList[k].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt)
             ansList[k].onclick()
