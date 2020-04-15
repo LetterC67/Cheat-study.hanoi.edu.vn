fd1663f3d6fdc047d07dd9e9b035e1ac0f04787a
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-04-15 22:50:11 +0700

    DEBUGGED!!

--- auto.js
+++ auto.js
@@ -1,15 +1,25 @@
-ans = JSON.parse(localStorage.getItem("ans"))
 var buttons = document.querySelectorAll('input[type="radio"]')
+desiredPoint = prompt("Nhập số điểm tối thiểu","10")
 x = document.getElementsByClassName("question-box")
+pointPerAns = 10/x.length
+cPoint = 0
+ans = JSON.parse(localStorage.getItem("ans"))
 for(i=0;i<x.length;i++){
-    ques = x[i].getElementsByClassName("col-11 question-box-title")[0].innerText.replace(/(\n| |\$)/gm, "");
+    if(cPoint >= desiredPoint)
+        break
+    ques = x[i].getElementsByClassName("col-11 question-box-title")[0].innerText.replace(/(\n| |\t|\$)/gm, "")
+    try{
+        ques += x[i].getElementsByTagName("img")[0].alt
+    }catch{
+    }
     ansList = x[i].querySelectorAll('input[type="radio"]')
     ansList2 = []
-    for(l=0;l<4;l++){
-        if(ansList[l].parentNode.nextElementSibling.getElementsByTagName("img").length)
+    for(l=0;l<ansList.length;l++){
+        try{
             ansList2.push(ansList[l].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt)
-        else
-            ansList2.push(ansList[l].parentNode.nextElementSibling.innerText.replace(/(\n| |\t)/gm, ""))
+        }catch{
+            ansList2.push(ansList[l].parentNode.nextElementSibling.innerText.replace(/(\n| |\t|\$)/gm, ""))
+        }
     }
     var j=0;
     for(j=0;j<ans.length;j++){
@@ -17,14 +27,22 @@ for(i=0;i<x.length;i++){
             break;
     }
     if(j==x.length){
+        for(j=0;j<ans.length;j++){
+            if(ansList2.includes(ans[j][1]))
+                break;
+        }
+    }
+    try{
+        for(k=0;k<ansList.length;k++){
+            if(ansList[k].parentNode.nextElementSibling.getElementsByTagName("img").length && ans[j][1] == ansList[k].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt)
+                ansList[k].onclick()
+            else if(ans[j][1] == ansList[k].parentNode.nextElementSibling.innerText.replace(/(\n| |\t|\$)/gm, ""))
+                ansList[k].onclick()
+        }
+        cPoint += pointPerAns
+    }catch{
         console.log("Có vấn đề với câu " + (i+1).toString() + ". Bạn có thể tự làm lại câu đó nhé!")
         continue
     }
-   
-    for(k=0;k<4;k++){
-        if(ansList[k].parentNode.nextElementSibling.getElementsByTagName("img").length && ans[j][1] == ansList[k].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt)
-            ansList[k].onclick()
-        else if(ans[j][1] == ansList[k].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, ""))
-            ansList[k].onclick()
-    }
 }
+alert("Auto study.hanoi.edu.vn by C67 (♕RED♚QUEEN♕)\n Đáp án đúng đã được chọn!")
