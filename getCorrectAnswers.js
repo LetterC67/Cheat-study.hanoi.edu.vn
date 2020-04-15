9141d96790c257bf104f2bc67bcd029f95e38188
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-04-15 22:50:37 +0700

    DEBUGGED!!

--- getCorrectAnswers.js
+++ getCorrectAnswers.js
@@ -1,48 +1,23 @@
-var buttons = document.querySelectorAll('input[type="radio"]')
-desiredPoint = prompt("Nhập số điểm tối thiểu","10")
-x = document.getElementsByClassName("question-box")
-pointPerAns = 10/x.length
-cPoint = 0
-ans = JSON.parse(localStorage.getItem("ans"))
+var ans = [];
+var x = document.getElementsByClassName("question-box");
 for(i=0;i<x.length;i++){
-    if(cPoint >= desiredPoint)
-        break
-    ques = x[i].getElementsByClassName("col-11 question-box-title")[0].innerText.replace(/(\n| |\t|\$)/gm, "")
-    try{
-        ques += x[i].getElementsByTagName("img")[0].alt
-    }catch{
-    }
-    ansList = x[i].querySelectorAll('input[type="radio"]')
-    ansList2 = []
-    for(l=0;l<ansList.length;l++){
-        try{
-            ansList2.push(ansList[l].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt)
-        }catch{
-            ansList2.push(ansList[l].parentNode.nextElementSibling.innerText.replace(/(\n| |\t|\$)/gm, ""))
-        }
-    }
-    var j=0;
-    for(j=0;j<ans.length;j++){
-        if(ques == ans[j][0] && ansList2.includes(ans[j][1]))
-            break;
-    }
-    if(j==x.length){
-        for(j=0;j<ans.length;j++){
-            if(ansList2.includes(ans[j][1]))
-                break;
-        }
-    }
-    try{
-        for(k=0;k<ansList.length;k++){
-            if(ansList[k].parentNode.nextElementSibling.getElementsByTagName("img").length && ans[j][1] == ansList[k].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt)
-                ansList[k].onclick()
-            else if(ans[j][1] == ansList[k].parentNode.nextElementSibling.innerText.replace(/(\n| |\t|\$)/gm, ""))
-                ansList[k].onclick()
-        }
-        cPoint += pointPerAns
-    }catch{
-        console.log("Có vấn đề với câu " + (i+1).toString() + ". Bạn có thể tự làm lại câu đó nhé!")
-        continue
-    }
+	ques = x[i].getElementsByClassName("col-11 question-box-title")[0].innerText.replace(/(\n| |\$|\t)/gm, "");
+	try{
+		ques += x[i].getElementsByTagName("img")[0].alt
+	}catch{}
+	try{
+		try{
+			ans.push([ques,x[i].getElementsByClassName("text-primary")[0].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt]);
+			continue;
+		}catch{
+			ans.push([ques,x[i].getElementsByClassName("text-success")[1].parentNode.nextElementSibling.getElementsByTagName("img")[0].alt]);
+			continue
+		}
+	}catch{}
+	try{
+		ans.push([ques,x[i].getElementsByClassName("text-primary")[0].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "")])
+	}catch{
+		ans.push([ques,x[i].getElementsByClassName("text-success")[1].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "")])
+	}
 }
-alert("Auto study.hanoi.edu.vn by C67 (♕RED♚QUEEN♕)\n Đáp án đúng đã được chọn!")
+localStorage.setItem("ans",JSON.stringify(ans))
