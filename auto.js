c4bcb38e44b2af1279ddfe0ed7d1858b143303d2
LetterC67 <53361407+LetterC67@users.noreply.github.com>
2020-04-18 15:36:30 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -3,7 +3,7 @@ desiredPoint = prompt("Nhập số điểm tối thiểu","10")
 x = document.getElementsByClassName("question-box")
 pointPerAns = 10/x.length
 cPoint = 0
-ans = JSON.parse(prompt("Paste đáp án phần vừa copy được vào đây"))
+ans = JSON.parse(sessionStorage.getItem("ans"))
 for(i=0;i<x.length;i++){
     if(cPoint >= desiredPoint)
         break
